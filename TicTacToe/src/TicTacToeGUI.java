a2a24ffea66d0ca420572b520509aee67ad4eff2
Sandro Baumgartner <bausac16@htl-kaindorf.ac.at>
2018-11-30 12:31:42 +0100

    added labels

--- TicTacToe/src/TicTacToeGUI.java
+++ TicTacToe/src/TicTacToeGUI.java
@@ -1,20 +1,30 @@
 
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.HeadlessException;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JPopupMenu;
+import javax.swing.JLabel;
 
 public class TicTacToeGUI extends JFrame{
 
     public TicTacToeGUI() throws HeadlessException {
         this.setSize(800,600);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        JPopupMenu popMenu = new JPopupMenu();
-        JMenu menu = new JMenu();
-        JMenuItem item = new JMenuItem("Close");
-        popMenu.add(item);
-        this.add(popMenu);
+        this.setLayout(new GridLayout(3,3));
+        
+        JLabel[] labels = new JLabel[9];
+        
+        for(int i = 0; i < labels.length; i++){
+            labels[i] = new JLabel();
+            labels[i].setBackground(Color.black);
+            labels[i].setOpaque(true);
+            labels[i].setBorder(BorderFactory.createLineBorder(Color.white));
+            this.add(labels[i]);
+        }
     }
     
     public static void main(String[] args) {
